06fb4ecff5bc1ad3eab72d7d7f6281463c69b349
bosummz1884 <Brandonjulian16@gmail.com>
2024-03-26 00:03:19 -0500

    nevermore

--- hello.js
+++ hello.js
@@ -4,7 +4,8 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("print two messages" , "on the same line");
+console.log('print a line with a quote inside of of like "Nevermore!"');
+
 
 
 
